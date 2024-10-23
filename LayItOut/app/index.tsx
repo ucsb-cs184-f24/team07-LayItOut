b01ea715866a90a40ace17f1dbc0b699db0fbd08
vaani arora <91294025+vaaniarora@users.noreply.github.com>
2024-10-23 15:09:56 -0700

    working on splash page

--- LayItOut/app/index.tsx
+++ LayItOut/app/index.tsx
@@ -1,1 +1,13 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import SplashPage from './pages/SplashPage.js'; 
+import { View } from 'react-native';  // Import View from react-native
+
+function App() {
+  return (
+    <View style={{ flex: 1 }}>
+      <SplashPage />
+    </View>  
+  );
+}
+
+export default App;
