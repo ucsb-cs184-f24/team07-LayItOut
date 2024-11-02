b0d2fed2c1e859a584784acfa6a441eeb4c96b5d
Palvi <psabherwal@ucsb.edu>
2024-11-02 00:03:12 -0700

    adding screenshot button

--- LayItOut/app/index.tsx
+++ LayItOut/app/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import LongRectangleRoom from './pages/LongRectangleRoom.js'; 
+import RectangleRoom from './pages/RectangleRoom.js';
+import CreatePage from './pages/CreatePage.js';
+import SquareRoom from './pages/SquareRoom.js';
+
 import { View } from 'react-native';  // Import View from react-native
 
 function App() {
   return (
     <View style={{ flex: 1 }}>
-      <LongRectangleRoom />
+      <SquareRoom/>
     </View>  
   );
 }
